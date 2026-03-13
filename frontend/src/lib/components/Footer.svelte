a4f990c6c02a9f552310e9b92dd26936f7df8bb9
Giovanni <gfeltrin25@gmail.com>
2026-03-13 10:49:09 +0100

    fix: change footer year

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -91,7 +91,7 @@
         </div>
 
         <div class="border-t border-gray-100 pt-6 text-xs text-gray-400">
-            © {new Date().getFullYear()} background annihilator. All rights reserved.
+            © 1984 background annihilator. All rights reserved.
         </div>
     </div>
 </footer>
